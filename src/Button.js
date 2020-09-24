3a21ec97540af94516dd23f4f6a2092bc1e37d04
Andrej Lamov <andrej.lamov@gmail.com>
2020-09-24 22:05:16 +0200

    Making a lib

---
+++ src/Button.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import { Button } from '@material-ui/core'
+
+export const HelloButton = () => (<Button>Hello</Button>);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import {Map} from 'immutable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import {HelloButton} from './Button.js';
 
 const data = Map({msg: "Hello dev server!"});
 
 ReactDOM.render(
-    <h1 className={['hello']}>{data.toString()}</h1>,
-    document.getElementById('root')
+  <HelloButton />,
+  document.getElementById('root')
 );
